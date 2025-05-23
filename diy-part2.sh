cbd2ccb148b4f3477bf71dc741141d9d34666459
3652365 <desmond.beyond@gmail.com>
2025-05-23 16:42:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.182/g' package/base-files/files/bin/config_generate
-mkdir package/tt/
 #git clone https://github.com/pymumu/luci-app-smartdns.git feeds/packages/net/luci-app-smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns.git luci/applications/openwrt-smartdns
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/tt/openwrt-passwall
@@ -20,5 +19,11 @@ mkdir package/tt/
 #git clone https://github.com/netdata/netdata.git package/tt/netdata
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/tt/luci-app-adguardhome
 #git clone https://github.com/AdguardTeam/AdGuardHome.git package/tt/AdGuardHome
-
-cp -r /tmp/luci-app-openclash/luci-app-openclash package/tt
+mkdir -p package/tt/luci-app-openclash
+cd package/tt/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
